f8c1ef5e608c67c0b4e1b7c37f4f38d73062892f
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-07-11 16:55:01 -0400

    Update system to steal

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var stealTools = require("steal-tools");
 
 stealTools.export({
-	system: {
+	steal: {
 		config: __dirname+"/package.json!npm"
 	},
 	outputs: {
@@ -9,16 +9,9 @@ stealTools.export({
 		"+global-js": {}
 	}
 }).catch(function(e){
-	
+
 	setTimeout(function(){
 		throw e;
 	},1);
-	
+
 });
-
-
-
-
-
-
-
